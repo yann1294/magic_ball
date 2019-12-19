f5f4a3b57355c777d8d5dc2ed3c27cd4cfc2ba46
yann1294 <dev.yann12@gmail.com>
2019-12-19 15:06:13 +0530

    Set Up
    Project Set Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,44 @@
 import 'package:flutter/material.dart';
 
 
+void main ()=> runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: MagicBallPage(),
+));
+
+class MagicBallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _BallPageState();
+  }
+}
+
+class _BallPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){},
+              child: Image.asset(
+                'images/ball1.png'
+              ),),),
+        ],
+      ),
+    );
+  }
+}
